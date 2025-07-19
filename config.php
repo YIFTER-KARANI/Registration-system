12f544468462cae2923d7bb56a754b2d76ed8c0a
YIFTER-KARANI <148689639+YIFTER-KARANI@users.noreply.github.com>
2025-07-19 17:56:26 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost";
+$username = "root"; // Change as needed
+$password = ""; // Change as needed
+$dbname = "registration_system";
+
+try {
+    $pdo = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+
+session_start();
+?>
